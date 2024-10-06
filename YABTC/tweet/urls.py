a13500671542821068d6d24ff2ab8001f3a46318
itsjazzkun <vanced.exe@gmail.com>
2024-10-06 18:11:42 +0530

    Updated models in tweet

--- YABTC/tweet/urls.py
+++ YABTC/tweet/urls.py
@@ -2,7 +2,9 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path('',views.index, name='index'),
 
-] 
+urlpatterns = [
+      path('', views.index, name='index'),  # Home view (or main page)
+      path('tweet/', views.index, name='tweet'),  # You may have a tweet view
+    # Add your other URL patterns here
+]
