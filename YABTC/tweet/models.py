4799fe89c9d6d0d6bcde7983a50c2891c3814df7
itsjazzkun <vanced.exe@gmail.com>
2024-10-08 23:38:08 +0530

    update models.py

--- YABTC/tweet/models.py
+++ YABTC/tweet/models.py
@@ -12,3 +12,13 @@ class Tweet(models.Model):
 
     def __str__(self):
         return f'{self.user.username} - {self.text[:10]}'
+    
+    def __str__(self):
+        if self.retweet:
+            return f'Retweet by {self.user.username} - {self.retweet.text[:10]}'
+        return f'{self.user.username} - {self.text[:10]}'
+
+    @property
+    def is_retweet(self):
+        return self.retweet is not None
+    
